c443972b4eb170522b004ce503f586ebc68f6a1a
philwilliamson <phil@philcodes.com>
2024-09-02 20:03:32 -0400

    Clean up model utils functions

--- src/scripts/util/model-utils.js
+++ src/scripts/util/model-utils.js
@@ -1,27 +1,36 @@
 export function buildSuperCell(crystal_model){
-	//construct supercell model based on unit cell model and supercell multipliers
-	//this function could be encapuslated in crytal model object
-	crystal_model.super_cell.a_hat = [crystal_model.unit_cell.a_hat[0] * crystal_model.super_cell.a_hat_multiplier,
-										crystal_model.unit_cell.a_hat[1] * crystal_model.super_cell.a_hat_multiplier,
-										crystal_model.unit_cell.a_hat[2] * crystal_model.super_cell.a_hat_multiplier];
-	crystal_model.super_cell.b_hat = [crystal_model.unit_cell.b_hat[0] * crystal_model.super_cell.b_hat_multiplier,
-										crystal_model.unit_cell.b_hat[1] * crystal_model.super_cell.b_hat_multiplier,
-										crystal_model.unit_cell.b_hat[2] * crystal_model.super_cell.b_hat_multiplier];
-	crystal_model.super_cell.c_hat = [crystal_model.unit_cell.c_hat[0] * crystal_model.super_cell.c_hat_multiplier,
-										crystal_model.unit_cell.c_hat[1] * crystal_model.super_cell.c_hat_multiplier,
-										crystal_model.unit_cell.c_hat[2] * crystal_model.super_cell.c_hat_multiplier];
+	// construct supercell model based on unit cell model and supercell multipliers
+	crystal_model.super_cell.a_hat = [
+        crystal_model.unit_cell.a_hat[0] * crystal_model.super_cell.a_hat_multiplier,
+        crystal_model.unit_cell.a_hat[1] * crystal_model.super_cell.a_hat_multiplier,
+        crystal_model.unit_cell.a_hat[2] * crystal_model.super_cell.a_hat_multiplier
+    ];
+
+	crystal_model.super_cell.b_hat = [
+        crystal_model.unit_cell.b_hat[0] * crystal_model.super_cell.b_hat_multiplier,
+        crystal_model.unit_cell.b_hat[1] * crystal_model.super_cell.b_hat_multiplier,
+        crystal_model.unit_cell.b_hat[2] * crystal_model.super_cell.b_hat_multiplier
+    ];
+
+	crystal_model.super_cell.c_hat = [
+        crystal_model.unit_cell.c_hat[0] * crystal_model.super_cell.c_hat_multiplier,
+        crystal_model.unit_cell.c_hat[1] * crystal_model.super_cell.c_hat_multiplier,
+        crystal_model.unit_cell.c_hat[2] * crystal_model.super_cell.c_hat_multiplier
+    ];
 	
-	//create atom list for super cell
+	// create atom list for super cell
 	crystal_model.super_cell.atoms = [];
 
 	for (var k = 0; k < crystal_model.super_cell.c_hat_multiplier; k++){
 		for (var j = 0; j < crystal_model.super_cell.b_hat_multiplier; j++){
 			for (var i = 0; i < crystal_model.super_cell.a_hat_multiplier; i++){
 				crystal_model.unit_cell.atoms.forEach(function(atom){
-					var super_cell_atom = {element: atom.element,
-											x_atom_pos: (atom.x_atom_pos + i) / crystal_model.super_cell.a_hat_multiplier,
-											y_atom_pos: (atom.y_atom_pos + j) / crystal_model.super_cell.b_hat_multiplier,
-											z_atom_pos: (atom.z_atom_pos + k) / crystal_model.super_cell.c_hat_multiplier}
+					var super_cell_atom = {
+                        element: atom.element,
+                        x_atom_pos: (atom.x_atom_pos + i) / crystal_model.super_cell.a_hat_multiplier,
+                        y_atom_pos: (atom.y_atom_pos + j) / crystal_model.super_cell.b_hat_multiplier,
+                        z_atom_pos: (atom.z_atom_pos + k) / crystal_model.super_cell.c_hat_multiplier
+                    };
 					crystal_model.super_cell.atoms.push(super_cell_atom);
 				});
 			}
@@ -31,7 +40,7 @@ export function buildSuperCell(crystal_model){
 };
 
 export function getElementColor(element){
-	//get element color based on Jmol coloring
+	// get element color
 	var defaultColor = [1, 0, 0, 1];
 
 	if (element_colors.hasOwnProperty(element)) {
@@ -154,5 +163,5 @@ const element_colors = {
     "Ds": [240/255,0/255,28/255, 1],
     "Rg": [245/255,0/255,18/255, 1],
     "Cn": [250/255,0/255,8/255, 1],
-    "Nh": [255/255,0/255,0/255, 1]
+    "Nh": [255/255,0/255,0/255, 1],
 };
